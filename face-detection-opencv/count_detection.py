67c6babc1813586ac7991db15bf35e9e51ab0cdb
Ishaan Sharma <ishaansharma711@gmail.com>
2023-08-27 14:56:44 +0530

    updated

--- face-detection-opencv/count_detection.py
+++ face-detection-opencv/count_detection.py
@@ -4,14 +4,23 @@ import cv2
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 image = cv2.imread('psg.jpg')
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
 faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.2, minNeighbors=8, minSize=(30, 30))
+
+num_faces = len(faces)
 
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = 1
+font_color = (255, 255, 255)  # White color
+face_count_text = f'Number of Faces: {num_faces}'
+cv2.putText(image, face_count_text, (10, 30), font, font_scale, font_color, 2)
+
 cv2.imshow('Face Detection', image)
 
 while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 cv2.destroyAllWindows()
